9a40b8b3431a5f1689bcd0f73fd3fc286ff8fe8f
Justin Samuels <blizm87@gmail.com>
2017-01-05 10:19:25 -0800

    completed callBackHell

--- filesnested.js
+++ filesnested.js
@@ -3,9 +3,62 @@
 */
 
 //require fs
+var fs = require('fs');
+//var fileData;
+//
+//var repeatCallback = function(){
+//  var fs = require('fs');
+//
+//}
+//
+//var textCallback = function(fileName){
+//  var fs = require('fs');
+//  fs.readFile( fileName,'utf-8',function(err, data){
+//    if(err) throw err;
+//    fileData += data;
+//    console.log(fileData);
+//    return textCallback;
+//  })
+//}
+//
+//var getFile = function(){
+//  var fs = require('fs');
+//  textCallback();
+//  console.log(fileData);
+//}
 
-
+//getFile('./file1.txt');
 //read file1
+fs.readFile('./file1.txt', 'utf-8', function(err, data){
+  if(err) throw err;
+  console.log(data);
+  var file1 = data;
+  fs.readFile('./file2.txt', 'utf-8', function(err, data){
+    if(err) throw err;
+    console.log(data);
+    var file2 = data;
+    fs.readFile('./file3.txt', 'utf-8', function(err, data){
+      if(err) throw err;
+      console.log(data);
+      var file3 = data;
+      var file4 = file1 + file2 + file3;
+      console.log(file4);
+      fs.writeFile('file4.txt', file4, function(err){
+        if(err) throw err;
+        console.log('Wrote file 4 saved.');
+        fs.readFile('./file4.txt', 'utf-8', function(err, data){
+          if(err) throw err;
+          var file4 = data + 'I am File4';
+          console.log(file4);
+          fs.writeFile('file5.txt', file4, function(err){
+          if(err) throw err;
+          console.log('file4 content was copied over to file5');
+          });
+        });
+      });
+    });
+  });
+});
     //read file2
         //read file3
           //join file1, fil2 and file3 contents
